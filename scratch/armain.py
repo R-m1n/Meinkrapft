86753ee053ae9f23e19bb05169fe4861c4fd9a5a
Armin Abbasi <apmnh.armin@gmail.com>
2025-05-10 12:53:21 -0700

    meooooowwww ffffffff

--- scratch/armain.py
+++ scratch/armain.py
@@ -16,9 +16,10 @@ class Perceptron:
         self.test_features = self.features[train_size:, :]
         self.test_targets = self.targets[train_size:].reshape(-1, 1)
 
-        self.weights = np.random.normal(loc=0, scale=0.01, size=(n_features, 1))
+        self.weights = np.random.normal(loc=0, scale=0.01, size=(1, n_features))
         self.bias = np.float64(0)
 
+        self.__net_input = lambda x: x @ self.weights.T + self.bias
         self.__activation = lambda x: np.where(x > 0, 1, 0)
 
     def fit(self, eta: float, epoch: int):
@@ -26,13 +27,13 @@ class Perceptron:
         confusions = np.empty(epoch)
 
         for i in range(epoch):
-            prediction = (self.train_features @ self.weights) + self.bias
+            net_input = self.__net_input(self.train_features)
 
-            activated = self.__activation(prediction)
+            activated = self.__activation(net_input)
 
             error = (self.train_targets - activated)
 
-            self.weights += eta * (self.train_features.T @ error)
+            self.weights += eta * (self.train_features.T @ error).T
             self.bias += eta * error.sum()
 
             confusions[i] = self.__test()
@@ -40,33 +41,34 @@ class Perceptron:
         return confusions
     
     def predict(self, x: np.ndarray):
-        prediction = (x @ self.weights) + self.bias
+        net_input = self.__net_input(x)
 
-        activated = self.__activation(prediction)
+        prediction = self.__activation(net_input)
 
-        return activated
+        return prediction
     
     def __test(self):
-        prediction = (self.test_features @ self.weights) + self.bias
+        net_input = self.__net_input(self.test_features)
 
-        activated = self.__activation(prediction)
+        activated = self.__activation(net_input)
 
         error = self.test_targets - activated
 
         return np.count_nonzero(error)
 
 class Adeline:
-    def __init__(self, features: pd.DataFrame, targets: pd.DataFrame, random_state: int = 42):
+    def __init__(self, features: pd.DataFrame, targets: pd.DataFrame, random_state: int = 42, train_size: float = 0.9):
         sample_size = features.shape[0]
         n_features = features.shape[1]
 
         self.__rng = np.random.default_rng(seed=random_state)
 
-        shuffle = self.__rng.permutation(range(len(features)))
+        self.__shuffle = lambda x: self.__rng.permutation(range(len(x)))
 
-        self.features, self.targets = features.to_numpy()[shuffle, :], targets.to_numpy()[shuffle]
+        self.features = features.to_numpy()[self.__shuffle(features), :]
+        self.targets = targets.to_numpy()[self.__shuffle(targets)]
 
-        self.train_size = int(sample_size * 0.9)
+        self.train_size = int(sample_size * train_size)
 
         self.train_features = self.features[:self.train_size, :]
         self.train_targets = self.targets[:self.train_size].reshape(-1, 1)
@@ -74,10 +76,12 @@ class Adeline:
         self.test_features = self.features[self.train_size:, :]
         self.test_targets = self.targets[self.train_size:].reshape(-1, 1)
 
-        self.weights = np.random.normal(loc=0, scale=0.01, size=(n_features, 1))
+        self.weights = np.random.normal(loc=0, scale=0.01, size=(1, n_features))
         self.bias = np.float64(0)
 
-        self.__activation = lambda x: np.where(x >= 0.5, 1, 0)
+        self.__net_input = lambda x: x @ self.weights.T + self.bias
+        self.__activation = lambda x: x
+        self.__threshold = lambda x: np.where(x >= 0.5, 1, 0)
 
     def fit(self, eta: float, epoch: int, batch_cut: int = 1):
 
@@ -86,24 +90,24 @@ class Adeline:
         train_loss, test_loss = np.empty(epoch), np.empty(epoch)
         
         for i in range(epoch):
-            shuffle = self.__rng.permutation(range(len(self.train_features)))
-
-            self.train_features = self.train_features[shuffle, :]
-            self.train_targets = self.train_targets[shuffle]
+            self.train_features = self.train_features[self.__shuffle(self.train_features), :]
+            self.train_targets = self.train_targets[self.__shuffle(self.train_targets)]
 
             errors = np.empty(batch_cut)
 
-            mini_batch_counter = 0
+            batch_counter = 0
             for j in range(0, len(self.train_features), batch_size):
-                prediction = (self.train_features[j: j + batch_size + 1, :] @ self.weights) + self.bias
+                net_input = self.__net_input(self.train_features[j: j + batch_size + 1, :])
 
-                error = (self.train_targets[j: j + batch_size + 1, :] - prediction)
+                activated = self.__activation(net_input)
 
-                self.weights += eta * ((self.train_features[j: j + batch_size + 1, :].T @ error) / self.train_size)
+                error = (self.train_targets[j: j + batch_size + 1, :] - activated)
+
+                self.weights += eta * ((self.train_features[j: j + batch_size + 1, :].T @ error) / self.train_size).T
                 self.bias += eta * error.mean()
 
-                errors[mini_batch_counter] = error.sum()
-                mini_batch_counter += 1
+                errors[batch_counter] = error.sum()
+                batch_counter += 1
 
             train_loss[i] = np.mean(np.square(errors)) / 2
             test_loss[i] = self.__test()
@@ -111,16 +115,18 @@ class Adeline:
         return train_loss, test_loss
     
     def predict(self, x: np.ndarray):
-        prediction = (x @ self.weights) + self.bias
+        net_input = self.__net_input(x)
 
-        activated = self.__activation(prediction)
+        activated = self.__activation(net_input)
 
-        return activated
+        prediction = self.__threshold(activated)
+
+        return prediction
         
     def __test(self):
-        prediction = (self.test_features @ self.weights) + self.bias
+        net_input = self.__net_input(self.test_features)
 
-        errors = (self.test_targets - prediction)
+        errors = (self.test_targets - net_input)
 
         test_loss = np.mean(np.square(errors)) / 2
 
